2e4cd3e021fbee7690e330a9c8006c2609064069
Khalil Motta <khalil.motta@discente.ufg.br>
2025-05-14 12:31:11 -0300

    Added test cases for newest server methods

--- client.py
+++ client.py
@@ -6,4 +6,8 @@ class Client:
   print (conn.root.exposed_value())
   conn.root.exposed_append(5)       # Call an exposed operation,
   conn.root.exposed_append(6)       # and append two elements
+  conn.root.exposed_append(11)
+  conn.root.exposed_append(9)
   print (conn.root.exposed_value())   # Print the result
+  print(conn.exposed_sum())
+  print(conn.exposed_showElement(2))
