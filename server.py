d8ebb8e0a5e1c7e532f5640948239d8871086c99
Khalil Motta <khalil.motta@discente.ufg.br>
2025-05-14 12:26:45 -0300

    Added two new methods in DBList class

--- server.py
+++ server.py
@@ -11,6 +11,19 @@ class DBList(rpyc.Service):
 
   def exposed_value(self):
     return self.value
+  
+  def exposed_sum(self):
+    sum = 0
+    for i in self.value:
+      sum += i
+    return sum
+  
+  def exposed_showElement(self, index):
+    if index >= self.value.length():
+      return "Error! Index out of bounds."
+    else:
+      return self.value[index]
+  
 
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
